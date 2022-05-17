2966f5f0805c7229759d19e24052d2aaf23a4a49
kikod4mn <kristo.leas@gmail.com>
2022-05-17 23:46:04 +0300

    Added logging slf4j and logback

--- jersey-api-server/src/main/java/com/kikopolis/server/JettyApiServer.java
+++ jersey-api-server/src/main/java/com/kikopolis/server/JettyApiServer.java
@@ -1,11 +1,16 @@
 package com.kikopolis.server;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 
 public class JettyApiServer {
+    private static final Logger logger = LoggerFactory.getLogger(JettyApiServer.class);
+    
     public static void main(String[] args) {
+        JettyApiServer.logger.info("HELLO WORLD");
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HTTPS.asString());
     }
