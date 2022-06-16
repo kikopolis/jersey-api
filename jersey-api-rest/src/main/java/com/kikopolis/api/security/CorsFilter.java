0a491bdba93ffb137ec9998a46d53287e0f34cf6
kikod4mn <kristo.leas@gmail.com>
2022-06-16 09:56:15 +0300

    Added hamcrest for testing collections. Added cors headers and cors test

---
+++ jersey-api-rest/src/main/java/com/kikopolis/api/security/CorsFilter.java
@@ -0,0 +1,17 @@
+package com.kikopolis.api.security;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) {
+        MultivaluedMap<String, Object> headers = containerResponseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD, PATCH");
+    }
+}
--- jersey-api-rest/src/test/java/com/kikopolis/api/security/CorsFilterTest.java
+++ jersey-api-rest/src/test/java/com/kikopolis/api/security/CorsFilterTest.java
@@ -1,21 +1,26 @@
-package com.kikopolis.api.resource;
+package com.kikopolis.api.security;
 
 import com.kikopolis.api.ApiApplication;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Response;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.jupiter.api.Assertions;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.Test;
 
-public class TestResourceTest extends JerseyTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CorsFilterTest extends JerseyTest {
     @Override
     protected Application configure() {
         return new ApiApplication();
     }
     
     @Test
-    public void test() {
+    public void corsHeaders() {
         Response response = target("/test").request().get();
         Assertions.assertEquals(200, response.getStatus());
+        assertThat(response.getHeaders(), IsMapContaining.hasKey("Access-Control-Allow-Origin"));
+        assertThat(response.getHeaders(), IsMapContaining.hasKey("Access-Control-Allow-Methods"));
     }
 }
